9b32d446e41c98bfaaabd29fa3eeeac5faed15bc
angelo <bezlotangelo@gmail.com>
2026-02-24 17:13:15 +1000

    initial core micro

---
+++ shortener-core/src/main/java/com/example/shortener_core/ShortenerCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.shortener_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenerCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenerCoreApplication.class, args);
+	}
+
+}
---
+++ shortener-core/src/test/java/com/example/shortener_core/ShortenerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shortener_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenerCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
